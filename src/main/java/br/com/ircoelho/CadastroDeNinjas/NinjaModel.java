d2e33f0f6a0e80c51e85c0689c78df1052704bd4
Igor Coelho <xh4rdzn@gmail.com>
2025-08-03 09:50:20 -0300

    refactor: Mudando o nome da classe

--- src/main/java/br/com/ircoelho/CadastroDeNinjas/NinjaModel.java
+++ src/main/java/br/com/ircoelho/CadastroDeNinjas/NinjaModel.java
@@ -10,8 +10,41 @@ public class NinjaModel {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
-  UUID id;
-  String name;
-  String email;
-  int idade;
+  private UUID id;
+  private String name;
+  private String email;
+  private int idade;
+
+  public NinjaModel(String name, String email, int idade) {
+    this.name = name;
+    this.email = email;
+    this.idade = idade;
+  }
+
+  public NinjaModel() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public int getIdade() {
+    return idade;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
 }
